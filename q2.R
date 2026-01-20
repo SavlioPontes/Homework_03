951b7d850603edf077918ef97c2a1b27ecf2230c
thaissbleal29 <thaisbleal2006@gmail.com>
2026-01-19 22:05:29 -0300

    q2

---
+++ q2.R
@@ -0,0 +1,5 @@
+# ----------- QUESTÃO 2 ----------- #
+install.packages("palmerpenguins"); library(palmerpenguins);
+penguins_data <-na.omit(penguins) # desconsiderando os dados faltantes
+
+# 1.1) PDF da distribuição exponencial
